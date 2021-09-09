1652a827d979e1d25065e80dbb81d7f9776b7c76
Mitchel Febechukwu <mitchellcorporations@gmail.com>
2021-09-09 11:36:05 +0100

    updated support for different screen sizes

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,39 +1,28 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 import '../controllers/home_controller.dart';
+import 'mobile/home_mobile_portrait.dart';
 
+/// this widget holds all the different screen sizes and it's responsible
+/// for splitting the views to their appropraite size.
+/// please take a look at the [responsive_builder] package for more
+/// understanding.
 class HomeView extends GetView<HomeController> {
-  final hc = Get.find<HomeController>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('HomeView'),
-        centerTitle: true,
+    return ScreenTypeLayout.builder(
+      mobile: (_) => OrientationLayoutBuilder(
+        portrait: (_) => HomeMobilePortrait(),
+        landscape: (_) =>
+            HomeMobilePortrait(), // replace with landscape view for mobile
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: hc.increament,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              'You\'ve clicked the button this many times',
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.headline6,
-            ),
-            Obx(
-              () => Text(
-                '${hc.count}',
-                style: Theme.of(context).textTheme.headline3,
-              ),
-            ),
-          ],
-        ),
+      tablet: (_) => OrientationLayoutBuilder(
+        portrait: (_) =>
+            HomeMobilePortrait(), // replace with portrait view for tablet
+        landscape: (_) =>
+            HomeMobilePortrait(), // replace with landscape view for tablet
       ),
     );
   }
